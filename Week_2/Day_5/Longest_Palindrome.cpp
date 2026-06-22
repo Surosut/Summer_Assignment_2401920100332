0b61c2e2149a292cbb55c4a968f3db3c71ae2865
Surosut <surosutsenior@gmail.com>
2026-06-22 19:41:35 +0530

    Create Longest_Palindrome.cpp

---
+++ Week_2/Day_5/Longest_Palindrome.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+    string longestPalindrome(string s)
+    {
+        int n = s.size();
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            // Odd palindrome
+            int l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r])
+            {
+                if (r - l + 1 > maxLen)
+                {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+
+            // Even palindrome
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r])
+            {
+                if (r - l + 1 > maxLen)
+                {
+                    maxLen = r - l + 1;
+                    start = l;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
